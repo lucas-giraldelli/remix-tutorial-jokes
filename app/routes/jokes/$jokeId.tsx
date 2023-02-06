0a616ae2235911d9bcefb6bce667166c03fc3113
Lucas Giraldelli <lucas.santos@treez.io>
2023-02-06 19:38:25 -0400

    more boundaries

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,6 +1,6 @@
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Link, useLoaderData, useParams } from '@remix-run/react';
+import { Link, useCatch, useLoaderData, useParams } from '@remix-run/react';
 
 import { db } from '~/utils/db.server';
 
@@ -9,7 +9,9 @@ export const loader = async ({ params }: LoaderArgs) => {
     where: { id: params.jokeId }
   });
   if (!joke) {
-    throw new Error('Joke not found');
+    throw new Response('What a joke! Not found.', {
+      status: 404
+    });
   }
   return json({ joke });
 };
@@ -26,11 +28,22 @@ export default function JokeRoute() {
   );
 }
 
+export function CatchBoundary() {
+  const caught = useCatch();
+  const params = useParams();
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">
+        Huh? What the heck is "{params.jokeId}"?
+      </div>
+    );
+  }
+  throw new Error(`Unhandled error: ${caught.status}`);
+}
+
 export function ErrorBoundary() {
   const { jokeId } = useParams();
   return (
-    <div className="error-container">
-      {`There was an error loading joke by the id ${jokeId}. Sorry.`}
-    </div>
+    <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
   );
 }
